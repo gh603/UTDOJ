c88110cf3be66ec09545067ffcc286e9966a81f4
gh603 <gehui603@gmail.com>
2018-03-29 16:23:29 -0500

    Add CSS Module

--- online-judge/src/App.js
+++ online-judge/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={classes.App}>
+        <header className={classes.App_header}>
+          <img src={logo} className={classes.App_logo} alt="logo" />
+          <h1 className={classes.App_title}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={classes.App_intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
