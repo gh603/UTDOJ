404ebe307db93abb7189f978e30336e9952f128e
carnivoreYY <yingyi814@gmail.com>
2018-04-03 15:45:48 -0500

    add Layout

--- online-judge/src/App.js
+++ online-judge/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import classes from './App.css';
+import Layout from "./hoc/Layout/Layout";
 
 class App extends Component {
   render() {
+      let routes = (
+          <div>
+              <p>yy</p>
+          </div>
+      );
+
     return (
-      <div className={classes.App}>
-        <header className={classes.App_header}>
-          <img src={logo} className={classes.App_logo} alt="logo" />
-          <h1 className={classes.App_title}>Welcome to React</h1>
-        </header>
-        <p className={classes.App_intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <Layout>
+                {routes}
+            </Layout>
+        </div>
     );
   }
 }
---
+++ online-judge/src/hoc/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import classes from './Layout.css';
+
+class Layout extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+                <div>
+                    <p>good</p>
+                </div>
+        )
+    }
+}
+
+export default Layout;
